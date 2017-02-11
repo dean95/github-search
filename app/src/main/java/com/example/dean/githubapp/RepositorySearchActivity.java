ae5fe70c4a54d8f835a7a63878824f5b945d2a3f
Dean Djermanović <dean74745@gmail.com>
2017-02-11 18:41:37 +0100

    Initial commit

---
+++ app/src/main/java/com/example/dean/githubapp/RepositorySearchActivity.java
@@ -0,0 +1,13 @@
+package com.example.dean.githubapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RepositorySearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository_search);
+    }
+}
